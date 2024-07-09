3c19b40aa013b761421b17b6ee524ef03c3b5bc9
Vinicius Vieira <vinicius2335@gmail.com>
2024-07-09 11:26:20 -0300

    feat(back): back end init

---
+++ back_end/planner/src/main/java/com/github/vinicius2335/planner/PlannerApplication.java
@@ -0,0 +1,13 @@
+package com.github.vinicius2335.planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannerApplication.class, args);
+	}
+
+}
---
+++ back_end/planner/src/test/java/com/github/vinicius2335/planner/PlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.vinicius2335.planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
