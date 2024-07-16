a38101c55fdce00d005c6a836299f7d208f03031
Vinicius Vieira <vinicius2335@gmail.com>
2024-07-16 15:39:02 -0300

    feat(mobile): final aula 01

---
+++ mobile/planner/note.md
@@ -0,0 +1,13 @@
+## Dependencias Instaladas
+- [Nativewind](https://www.nativewind.dev/v4/getting-started/expo-router)
+- [Expo Fonts](https://docs.expo.dev/develop/user-interface/fonts/)
+- [Clsx - aplica classes/variantes de forma condicional](https://www.npmjs.com/package/clsx)
+- [Lucide](https://lucide.dev/guide/packages/lucide-react-native)
+  - e react-native-svg pq os icones estão em svg
+
+## Não esquecer
+- como configurar as importaçoes em tsconfig.json
+- ``npx expo start``
+- arquivos com _ = arquivos de configuração das rotas de navegação
+- ``npx expo start --clear``
+- quebrar a linha ``{"\n"}``
---
+++ mobile/planner/src/components/button.tsx
@@ -0,0 +1,56 @@
+import { Text, TextProps, TouchableOpacity, TouchableOpacityProps, ActivityIndicator } from "react-native"
+import clsx from "clsx"
+import { createContext, useContext } from "react"
+
+type Variants = "primary" | "secondary"
+
+type ButtonProps = TouchableOpacityProps & {
+  variant?: Variants
+  isLoading?: boolean
+}
+
+// Contexto que existe somente para esse componente
+// assim Title tbm poderá ter a acesso a variante e aplicar os estilos de acordo com a opção selecionada 
+// Passo 1
+const ThemeContext = createContext<{variant ?: Variants}>({})
+
+function Button({ children, isLoading, variant = "primary", ...props }: ButtonProps) {
+  return (
+    <TouchableOpacity
+      className={clsx("w-full h-11 flex-row items-center justify-center rounded-lg gap-2", {
+        "bg-lime-300": variant === "primary",
+        "bg-zinc-800": variant === "secondary"
+      })}
+      activeOpacity={0.7}
+      disabled={isLoading}
+      {...props}>
+
+      {/* Passo 2 */}
+      <ThemeContext.Provider value={{variant}}>
+        { isLoading ? <ActivityIndicator className="text-lime-950" /> : children}
+      </ThemeContext.Provider>
+
+    </TouchableOpacity>
+  )
+}
+
+function Title({ ...props }: TextProps) {
+  // Passo 3
+  const { variant } = useContext(ThemeContext)
+
+  return (
+    <Text
+      className={clsx(
+        "text-base font-semibold",
+        {
+          "text-lime-950": variant === "primary",
+          "text-zinc-200": variant === "secondary"
+        }
+      )}
+      {...props}></Text>
+  )
+}
+
+Button.Title = Title
+
+export { Button }
